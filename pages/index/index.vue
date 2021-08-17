1ae7ec0fcb18571cb49ce1a155f7208a222b401e
Bryce <5174887+lubandiule@user.noreply.gitee.com>
2021-08-17 18:18:55 +0800

    first add

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		<view class="control">
+		</view>
+		
+		<view class="">
+			<slider value="50" @change="sliderChange" show-value class="volume"/>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				"volume": 20,
+			}
+		},
+		onLoad() { 
+	
+		},
+		methods: {
+			sliderChange(e) {
+				console.log('value 发生变化：' + e.detail.value)
+			}
+		}
+	}
+</script>
+
+<style>
+	.volume {
+		width: 750rpx;
+		margin: 0;
+	}
+</style>
